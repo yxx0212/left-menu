ba5dfbfad9dad6bbb5eb34f40ffdb76d836b75da
yaoxinxiu <yaoxinxiu@onest.net>
2018-04-24 16:32:47 +0800

    样式修改，点击二级菜单js修改

--- js/index.js
+++ js/index.js
@@ -4,7 +4,12 @@ $(document).ready(function(){
 		$(this).next().slideToggle();
 		$(this).parent(".nav-li").toggleClass('active-li');
 	});
+
+	//二级菜单点击事件
 	$(".item>a").on("click",function(){
-		$(this).prev("span").addClass('doc').parents("li").siblings().find("span").removeClass('doc');
-	})
+		/*$(this).prev("span").addClass('doc').parents("li").siblings().find("span").removeClass('doc');*/
+		
+		$(this).parents("ul.nav").find('li.active').removeClass('active');
+		$(this).parent("li.item").addClass('active');
+	});
 })
