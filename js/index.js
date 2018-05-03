6ce4597ea921cbe465b2f68b2b813e98d25e3907
yaoxinxiu <yaoxinxiu@onest.net>
2018-05-03 09:25:43 +0800

    左侧菜单修改，右侧添加表格

--- js/index.js
+++ js/index.js
@@ -1,17 +1,28 @@
+
 $(document).ready(function(){
 	//一级菜单的点击事件
 	$(".nav-a").on("click",function(){
-		$(this).next().slideToggle();
-		$(this).parent(".nav-li").toggleClass('active-li');
+		/*$(this).next().slideToggle();*/
+		$(this).parents("ul.nav-ul").find('li.active').removeClass('active');
+		$(this).parent("li").addClass('active');
+		$(this).parents("ul.nav-ul").find('li.active-sec').removeClass('active-sec');
+		console.log('使用webpack打包');
 	});
 
 	//二级菜单点击事件
 	$(".item>a").on("click",function(){
 		/*$(this).prev("span").addClass('doc').parents("li").siblings().find("span").removeClass('doc');*/
+		$(this).parents("ul.nav-ul").find('li.active').removeClass('active');
+		$(this).parents("li").addClass('active');
 		
-		$(this).parents("ul.nav").find('li.active').removeClass('active');
-		$(this).parent("li.item").addClass('active');
-		$(this).parents("ul.nav").find("li.spc").removeClass('spc');
-		$(this).parents("li.nav-li").addClass('spc');
+		$(this).parents("ul.nav-ul").find('li.active-sec').removeClass('active-sec');
+		$(this).parent("li.item").addClass('active-sec');
+		console.log('设置运行的快捷键');
+
 	});
+
+	$(".sec").on("click",function(){
+		$(this).next().slideToggle();
+		$(this).toggleClass('click-span');
+	})
 })
