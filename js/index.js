7958db8b95b934367b3a6e3dacb86e4fedff4e47
yaoxinxiu <yaoxinxiu@onest.net>
2018-05-04 14:15:31 +0800

    样式及结构调整

--- js/index.js
+++ js/index.js
@@ -6,23 +6,22 @@ $(document).ready(function(){
 		$(this).parents("ul.nav-ul").find('li.active').removeClass('active');
 		$(this).parent("li").addClass('active');
 		$(this).parents("ul.nav-ul").find('li.active-sec').removeClass('active-sec');
-		console.log('使用webpack打包');
 	});
 
 	//二级菜单点击事件
 	$(".item>a").on("click",function(){
 		/*$(this).prev("span").addClass('doc').parents("li").siblings().find("span").removeClass('doc');*/
 		$(this).parents("ul.nav-ul").find('li.active').removeClass('active');
-		$(this).parents("li").addClass('active');
+		$(this).parents("ul").prev("li").addClass('active');
 		
 		$(this).parents("ul.nav-ul").find('li.active-sec').removeClass('active-sec');
 		$(this).parent("li.item").addClass('active-sec');
-		console.log('设置运行的快捷键');
 
 	});
 
 	$(".sec").on("click",function(){
-		$(this).next().slideToggle();
+		$(this).parent('li').next().slideToggle();
 		$(this).toggleClass('click-span');
-	})
+	});
+
 })
