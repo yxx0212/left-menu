cf15b41190c5106264d2e69bc247f30f1a2932f1
yaoxinxiu <yaoxinxiu@onest.net>
2018-05-22 16:54:07 +0800

    Revert 撤销上一次提交
    This reverts commit 914f926c0ed28414225fb09246d95fa5943caaa5.

--- js/index.js
+++ js/index.js
@@ -3,9 +3,6 @@ function first_level(event){
 	obj.parents("div.list").siblings().find('div.active').removeClass('active');
 	obj.parent("div.first").addClass('active');
 	obj.parents("div.menu-list").find("a.active-sec").removeClass('active-sec'); //选中一级菜单将二级菜单样式去除
-
-	obj.parent().next().toggle(100);
-	obj.next().toggleClass('click-span');
 }
 
 function second_level(event){
@@ -26,11 +23,5 @@ function sec(event){
 $(document).ready(function(){
 	$(".item").on("click",first_level);
 	$(".second>a").on("click",second_level);
-
-	var flag2=$(".active");
-	if(flag2.find(".sec").length>0){
-		flag2.next().show();
-		$("span.sec").addClass('click-span');
-	}
-
+	$(".sec").on("click",sec);
 })
